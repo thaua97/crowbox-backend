be69ebb55cd1f9f6137891797dd8bb29849247ec
Thauã Borges <thaua97@gmail.com>
2019-04-10 17:03:48 -0300

    corrigindo URL

--- src/models/File.js
+++ src/models/File.js
@@ -20,7 +20,7 @@ const File = new mongoose.Schema({
 File.virtual('url').get(function() {
     const url = process.env.URL || 'http://localhost:3333/files'
     
-    return `${url}/${encodeURIComponent(this.path)}`
+    return `${url}/files/${encodeURIComponent(this.path)}`
 })
 
 module.exports = mongoose.model('File', File)
